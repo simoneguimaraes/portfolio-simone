c9a1047aae855e770c63f9a0f045bc821ca0fca9
simoneguimaraes <simoneguimaraes11@gmail.com>
2022-01-06 14:32:56 -0300

    insertion of cv information

--- src/components/CVSection/CVSection.js
+++ src/components/CVSection/CVSection.js
@@ -4,7 +4,7 @@ function CVSection() {
   return (
     <div className="cv-container">
       <h1 className="cv-title">SOFTWARE DEVELOPER</h1>
-      <p className="cv-tech">Text here</p>
+      <p className="cv-tech">Simone Guimaraes is a full-stack web developer, working with the MERN stack (MongoDB, ExpressJs, ReactJs, NodeJs).</p>
       <h2 className="cv-tech">Technical Skills</h2>
       <div className="border-cv"></div>
       <ul className="cv-list-itens">
@@ -28,73 +28,75 @@ function CVSection() {
       </ul>
       <h2 className="cv-tech">Education</h2>
       <div className="border-cv"></div>
-      {/* University */}
-      <div className="cv-company">
-        <h3 className="cv-tech">University</h3>
-        <h4 className="cv-tech"><em>apr/20 - aug/20</em></h4>
+    {/* University */}
+    <div className="cv-company">
+        <h3 className="cv-tech">Ironhack</h3>
+        <h4 className="cv-tech"><em>out/21 - dec/21</em></h4>
     </div>
         <ul className="cv-list-itens">
             <li>
-                Course
+            Web Development Bootcamp Graduate
             </li>
         </ul>
     {/* University */}
     <div className="cv-company">
-        <h3 className="cv-tech">University</h3>
-        <h4 className="cv-tech"><em>apr/20 - aug/20</em></h4>
+        <h3 className="cv-tech">Swinburne University of Technology</h3>
+        <h4 className="cv-tech"><em>jan/14 - jul/14</em></h4>
     </div>
         <ul className="cv-list-itens">
             <li>
-                Course
+                Business and Innovation Specialization
             </li>
         </ul>
     {/* University */}
     <div className="cv-company">
-        <h3 className="cv-tech">University</h3>
-        <h4 className="cv-tech"><em>apr/20 - aug/20</em></h4>
+        <h3 className="cv-tech">University of Sao Paulo (USP)</h3>
+        <h4 className="cv-tech"><em>feb/10 - jul/15</em></h4>
     </div>
         <ul className="cv-list-itens">
             <li>
-                Course
+                Bachelor of Business Administration
             </li>
         </ul>
-      <h2 className="cv-tech">Experience</h2>
-      <div className="border-cv"></div>
-      {/* Experience */}
-      <div className="cv-company">
-        <h3 className="cv-tech">Title - Company</h3>
-        <h4 className="cv-tech"><em>apr/20 - aug/20</em></h4>
-    </div>
-        <ul className="cv-list-itens">
-            <li>
-            JavaScript (ES6), ReactJs, NodeJs, ExpressJS, Insomnia, Bootstrap 5,
-            Netflify, Heroku, npm, Git;
-            </li>
-        </ul>
-    {/* Experience */}
-    <div className="cv-company">
-        <h3 className="cv-tech">Title - Company</h3>
-        <h4 className="cv-tech"><em>apr/20 - aug/20</em></h4>
-    </div>
-        <ul className="cv-list-itens">
-            <li>
-            JavaScript (ES6), ReactJs, NodeJs, ExpressJS, Insomnia, Bootstrap 5,
-            Netflify, Heroku, npm, Git;
-            </li>
-        </ul>
-    {/* Experience */}
-    <div className="cv-company">
-        <h3 className="cv-tech">Title - Company</h3>
-        <h4 className="cv-tech"><em>apr/20 - aug/20</em></h4>
-    </div>
-        <ul className="cv-list-itens">
-            <li>
-            JavaScript (ES6), ReactJs, NodeJs, ExpressJS, Insomnia, Bootstrap 5,
-            Netflify, Heroku, npm, Git;
-            </li>
-        </ul>
+
     </div>
   );
 }
 
 export default CVSection;
+
+// <h2 className="cv-tech">Experience</h2>
+// <div className="border-cv"></div>
+// {/* Experience */}
+// <div className="cv-company">
+//   <h3 className="cv-tech">Title - Company</h3>
+//   <h4 className="cv-tech"><em>apr/20 - aug/20</em></h4>
+// </div>
+//   <ul className="cv-list-itens">
+//       <li>
+//       JavaScript (ES6), ReactJs, NodeJs, ExpressJS, Insomnia, Bootstrap 5,
+//       Netflify, Heroku, npm, Git;
+//       </li>
+//   </ul>
+// {/* Experience */}
+// <div className="cv-company">
+//   <h3 className="cv-tech">Title - Company</h3>
+//   <h4 className="cv-tech"><em>apr/20 - aug/20</em></h4>
+// </div>
+//   <ul className="cv-list-itens">
+//       <li>
+//       JavaScript (ES6), ReactJs, NodeJs, ExpressJS, Insomnia, Bootstrap 5,
+//       Netflify, Heroku, npm, Git;
+//       </li>
+//   </ul>
+// {/* Experience */}
+// <div className="cv-company">
+//   <h3 className="cv-tech">Title - Company</h3>
+//   <h4 className="cv-tech"><em>apr/20 - aug/20</em></h4>
+// </div>
+//   <ul className="cv-list-itens">
+//       <li>
+//       JavaScript (ES6), ReactJs, NodeJs, ExpressJS, Insomnia, Bootstrap 5,
+//       Netflify, Heroku, npm, Git;
+//       </li>
+//   </ul>
