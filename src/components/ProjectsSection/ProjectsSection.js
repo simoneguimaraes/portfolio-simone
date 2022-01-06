e404636b582e8f4759e4caf6cfb32186fa1e7985
simoneguimaraes <simoneguimaraes11@gmail.com>
2022-01-06 10:31:21 -0300

    inserted project two and three

--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -3,6 +3,7 @@ import "./ProjectsSection.css";
 function ProjectsSection() {
   return (
     <div className="project-container">
+      {/* PROJECT ONE */}
       <h1 className="project-title">BLOOM</h1>
       <p>
         Bloom is a full-stack aplication that tracks the daily use of medicinal
@@ -15,7 +16,7 @@ function ProjectsSection() {
         Application (SPA).
       </p>
       <h2 className="project-about">About this project</h2>
-      <div className="border"></div>
+      <div className="border-project"></div>
       <ul className="project-list-itens">
         <li>
           The project was made from scratch, so I was responsible for end-to-end
@@ -27,7 +28,7 @@ function ProjectsSection() {
         </li>
       </ul>
       <h2 className="project-challenge">Technical Challenges</h2>
-      <div className="border"></div>
+      <div className="border-project"></div>
       <ul className="project-list-itens">
         <li>
           The routes for Doctor Profile and Patient Profile have unique features
@@ -38,7 +39,7 @@ function ProjectsSection() {
       </ul>
       <h2 className="project-tech">Technologies used</h2>
 
-      <div className="border"></div>
+      <div className="border-project"></div>
       <ul className="project-list-itens">
         <li>JavaScript (ES6), ReactJs, NodeJs, HTML5 e CSS3;</li>
         <li>Relational databased (MongoDB) and ODMs;</li>
@@ -59,10 +60,165 @@ function ProjectsSection() {
         <button className="project-button">
           <a
             className="project-link"
-            href="https://github.com/simoneguimaraes/app-bloom-client" target="_blank"
+            href="https://github.com/simoneguimaraes/app-bloom-server" target="_blank"
             rel="noreferrer"
           >
-            Visit Github
+            Visit Source Code
+          </a>
+        </button>
+        {/* <button className="project-button">
+          <a
+            className="project-link"
+            href="https://cacildis-viagens.netlify.app/" target="_blank"
+            rel="noreferrer"
+          >
+            Visit Website
+          </a>
+        </button> */}
+      </div>
+      {/* PROJECT TWO */}
+      <h1 className="project-title">CACILDIS VIAGENS</h1>
+      <p>
+        Bloom is a full-stack aplication that tracks the daily use of medicinal
+        cannabidiol (CBD) by patients, gives this information for doctors to
+        promote future research and creates a community using forums and new
+        articles published.
+      </p>
+      <p>
+        MERN Full-Stack (MongoDB, ExpressJs, ReactJs, Node.js) Single Page
+        Application (SPA).
+      </p>
+      <h2 className="project-about">About this project</h2>
+      <div className="border-project"></div>
+      <ul className="project-list-itens">
+        <li>
+          The project was made from scratch, so I was responsible for end-to-end
+          development and deploy of the SPA.
+        </li>
+        <li>
+          Responsible for the modeling the database, creating routes, front-end,
+          back-end, UI, UX of this project.
+        </li>
+      </ul>
+      <h2 className="project-challenge">Technical Challenges</h2>
+      <div className="border-project"></div>
+      <ul className="project-list-itens">
+        <li>
+          The routes for Doctor Profile and Patient Profile have unique features
+          since they have different use for the application.
+          So the main challenge was to develop two different structures for the
+          application, depending on who was login in.
+        </li>
+      </ul>
+      <h2 className="project-tech">Technologies used</h2>
+
+      <div className="border-project"></div>
+      <ul className="project-list-itens">
+        <li>JavaScript (ES6), ReactJs, NodeJs, HTML5 e CSS3;</li>
+        <li>Relational databased (MongoDB) and ODMs;</li>
+        <li>
+          Clean, modular and efficient code and develop well-tested applications
+          using TDD principles;
+        </li>
+        <li>Integration of the 3rd party services (APIs);</li>
+        <li>Servers and Hosting;</li>
+        <li>
+          Responsive Web Design, Fundamentals of Object-oriented programming
+          based on JavaScript;
+        </li>
+        <li>Git + Github and Version control;</li>
+        <li>Agile tools and methodologies, sprints.</li>
+      </ul>
+      <div className="project-button-div">
+        <button className="project-button">
+          <a
+            className="project-link"
+            href="https://github.com/simoneguimaraes/Cacildis-Viagens" target="_blank"
+            rel="noreferrer"
+          >
+            Visit Source Code
+          </a>
+        </button>
+        <button className="project-button">
+          <a
+            className="project-link"
+            href="https://cacildis-viagens.netlify.app/" target="_blank"
+            rel="noreferrer"
+          >
+            Visit Website
+          </a>
+        </button>
+      </div>
+            {/* PROJECT THREE */}
+            <h1 className="project-title">GAME SHOW DO TIOZAO</h1>
+      <p>
+        Bloom is a full-stack aplication that tracks the daily use of medicinal
+        cannabidiol (CBD) by patients, gives this information for doctors to
+        promote future research and creates a community using forums and new
+        articles published.
+      </p>
+      <p>
+        MERN Full-Stack (MongoDB, ExpressJs, ReactJs, Node.js) Single Page
+        Application (SPA).
+      </p>
+      <h2 className="project-about">About this project</h2>
+      <div className="border-project"></div>
+      <ul className="project-list-itens">
+        <li>
+          The project was made from scratch, so I was responsible for end-to-end
+          development and deploy of the SPA.
+        </li>
+        <li>
+          Responsible for the modeling the database, creating routes, front-end,
+          back-end, UI, UX of this project.
+        </li>
+      </ul>
+      <h2 className="project-challenge">Technical Challenges</h2>
+      <div className="border-project"></div>
+      <ul className="project-list-itens">
+        <li>
+          The routes for Doctor Profile and Patient Profile have unique features
+          since they have different use for the application.
+          So the main challenge was to develop two different structures for the
+          application, depending on who was login in.
+        </li>
+      </ul>
+      <h2 className="project-tech">Technologies used</h2>
+
+      <div className="border-project"></div>
+      <ul className="project-list-itens">
+        <li>JavaScript (ES6), ReactJs, NodeJs, HTML5 e CSS3;</li>
+        <li>Relational databased (MongoDB) and ODMs;</li>
+        <li>
+          Clean, modular and efficient code and develop well-tested applications
+          using TDD principles;
+        </li>
+        <li>Integration of the 3rd party services (APIs);</li>
+        <li>Servers and Hosting;</li>
+        <li>
+          Responsive Web Design, Fundamentals of Object-oriented programming
+          based on JavaScript;
+        </li>
+        <li>Git + Github and Version control;</li>
+        <li>Agile tools and methodologies, sprints.</li>
+      </ul>
+      <div className="project-button-div">
+        <button className="project-button">
+          <a
+            className="project-link"
+            href="https://github.com/simoneguimaraes/game-show-do-tiozao" target="_blank"
+            rel="noreferrer"
+          >
+            Visit Source Code
+          </a>
+        </button>
+        <button className="project-button">
+          <a
+            className="project-link"
+            href="https://simoneguimaraes.github.io/game-show-do-tiozao/" target="_blank"
+            rel="noreferrer"
+          >
+            Visit Website
           </a>
         </button>
       </div>
