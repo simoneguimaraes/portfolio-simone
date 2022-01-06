91e38d7b7e22a5a3c8ce0271b38c61045d8876ff
simoneguimaraes <simoneguimaraes11@gmail.com>
2022-01-06 11:36:07 -0300

    updated info in project three

--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -19,7 +19,7 @@ function ProjectsSection() {
       <div className="border-project"></div>
       <ul className="project-list-itens">
         <li>
-          The project was made from scratch, so I was responsible for end-to-end
+          The project was made from scratch in one week, so I was responsible for end-to-end
           development and deploy of the SPA.
         </li>
         <li>
@@ -41,7 +41,7 @@ function ProjectsSection() {
 
       <div className="border-project"></div>
       <ul className="project-list-itens">
-        <li>JavaScript (ES6), ReactJs, NodeJs, Insomnia, Bootstrap 5;</li>
+        <li>JavaScript (ES6), ReactJs, NodeJs, ExpressJS, Insomnia, Bootstrap 5, Netflify, Heroku, npm, Git;</li>
         <li>Relational databased (MongoDB) and ODMs;</li>
         <li>
           Clean, modular and efficient code and develop well-tested applications
@@ -151,54 +151,34 @@ function ProjectsSection() {
       {/* PROJECT THREE */}
       <h1 className="project-title">GAME SHOW DO TIOZAO</h1>
       <p>
-        Bloom is a full-stack aplication that tracks the daily use of medicinal
-        cannabidiol (CBD) by patients, gives this information for doctors to
-        promote future research and creates a community using forums and new
-        articles published.
+        This application is a game developed in one week during my Ironhack Web Development Bootcamp.
+        I have always enjoyed trivia games so it was a great and fun challenge to develop it.
       </p>
       <p>
-        MERN Full-Stack (MongoDB, ExpressJs, ReactJs, Node.js) Single Page
-        Application (SPA).
+        Developed using JavaScript, pure CSS and HTML.
       </p>
       <h2 className="project-about">About this project</h2>
       <div className="border-project"></div>
       <ul className="project-list-itens">
         <li>
-          The project was made from scratch, so I was responsible for end-to-end
-          development and deploy of the SPA.
-        </li>
-        <li>
-          Responsible for the modeling the database, creating routes, front-end,
-          back-end, UI, UX of this project.
+          The project was made from scratch in one, so I was responsible for end-to-end
+          development and deploy of the game.
         </li>
       </ul>
       <h2 className="project-challenge">Technical Challenges</h2>
       <div className="border-project"></div>
       <ul className="project-list-itens">
-        <li>
-          The routes for Doctor Profile and Patient Profile have unique features
-          since they have different use for the application. So the main
-          challenge was to develop two different structures for the application,
-          depending on who was login in.
-        </li>
+        <li>Identify each element in the DOM is and how to select it to use in functions;</li>
+        <li>How to insert new features without breaking the code;</li>
+        <li>How to keep a clean clean with so many variables and DOM elements;</li>
+        <li>What is the most important thing to do first.</li>
       </ul>
       <h2 className="project-tech">Technologies used</h2>
-
       <div className="border-project"></div>
       <ul className="project-list-itens">
-        <li>JavaScript (ES6), ReactJs, NodeJs, HTML5 e CSS3;</li>
-        <li>Relational databased (MongoDB) and ODMs;</li>
-        <li>
-          Clean, modular and efficient code and develop well-tested applications
-          using TDD principles;
-        </li>
-        <li>Integration of the 3rd party services (APIs);</li>
-        <li>Servers and Hosting;</li>
-        <li>
-          Responsive Web Design, Fundamentals of Object-oriented programming
-          based on JavaScript;
-        </li>
-        <li>Git + Github and Version control;</li>
+        <li>JavaScript (ES6), HTML5 e CSS3;</li>
+        <li>Mobile-first and Responsive Web Design;</li>
+        <li>Fundamentals of Object-oriented programming based on JavaScript;</li>
         <li>Agile tools and methodologies, sprints.</li>
       </ul>
       <div className="project-button-div">
